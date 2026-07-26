272597689a8721d7f5742a71555ba86ddb953cbd
Rares Bogdan <bogdan.andrei.rares@gmail.com>
2026-07-26 13:33:58 +0300

    Add debug config

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -1,164 +1,200 @@
 return {
-	"mfussenegger/nvim-dap",
+    "mfussenegger/nvim-dap",
 
-	enabled = true,
+    enabled = true,
 
-	dependencies = {
-		"rcarriga/nvim-dap-ui",
-		"nvim-neotest/nvim-nio",
-		"theHamsta/nvim-dap-virtual-text",
-		"williamboman/mason.nvim",
-		"jay-babu/mason-nvim-dap.nvim",
-		"mfussenegger/nvim-dap-python",
-	},
-	keys = {
-		{
-			"<F5>",
-			function()
-				require("dap").continue()
-			end,
-			desc = "Debug: Start/Continue",
-		},
-		{
-			"<F1>",
-			function()
-				require("dap").step_into()
-			end,
-			desc = "Debug: Step Into",
-		},
-		{
-			"<F2>",
-			function()
-				require("dap").step_over()
-			end,
-			desc = "Debug: Step Over",
-		},
-		{
-			"<F3>",
-			function()
-				require("dap").step_out()
-			end,
-			desc = "Debug: Step Out",
-		},
-		{
-			"<leader>b",
-			function()
-				require("dap").toggle_breakpoint()
-			end,
-			desc = "Debug: Toggle Breakpoint",
-		},
-		{
-			"<leader>B",
-			function()
-				require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: "))
-			end,
-			desc = "Debug: Set Breakpoint",
-		},
-		{
-			"<F7>",
-			function()
-				require("dapui").toggle()
-			end,
-			desc = "Debug: See last session result.",
-		},
-	},
-	config = function()
-		local dap = require("dap")
-		local dapui = require("dapui")
+    dependencies = {
+        "rcarriga/nvim-dap-ui",
+        "nvim-neotest/nvim-nio",
+        "theHamsta/nvim-dap-virtual-text",
+        "williamboman/mason.nvim",
+        "jay-babu/mason-nvim-dap.nvim",
+        "mfussenegger/nvim-dap-python",
+    },
+    keys = {
+        {
+            "<F5>",
+            function()
+                require("dap").continue()
+            end,
+            desc = "Debug: Start/Continue",
+        },
+        {
+            "<Up>",
+            function()
+                require("dap").continue()
+            end,
+            desc = "Debug: Start/Continue",
+        },
+        {
+            "<Right>",
+            function()
+                require("dap").step_into()
+            end,
+            desc = "Debug: Step Into",
+        },
+        {
+            "<Down>",
+            function()
+                require("dap").step_over()
+            end,
+            desc = "Debug: Step Over",
+        },
+        {
+            "<Left>",
+            function()
+                require("dap").step_out()
+            end,
+            desc = "Debug: Step Out",
+        },
+        {
+            "<leader>de",
+            function()
+                local dap = require("dap")
+                dap.clear_breakpoints()
+                dap.run({
+                    name = "Launch",
+                    type = "codelldb",
+                    request = "launch",
+                    program = function()
+                        return vim.fn.input("Executable: ", vim.fn.getcwd() .. "/", "file")
+                    end,
+                    cwd = "${workspaceFolder}",
+                })
+            end,
+            desc = "Debug: Choose executable",
+        },
+        {
+            "<leader>db",
+            function()
+                require("dap").toggle_breakpoint()
+            end,
+            desc = "Debug: Toggle Breakpoint",
+        },
+        {
+            "<leader>dB",
+            function()
+                require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: "))
+            end,
+            desc = "Debug: Set Breakpoint",
+        },
+        {
+            "<leader>dr",
+            function()
+                require("dap").restart()
+            end,
+            desc = "Debug: Restart",
+        },
+        {
+            "<leader>dq",
+            function()
+                require("dap").terminate()
+            end,
+            desc = "Debug: Stop",
+        },
+        {
+            "<leader>du",
+            function()
+                require("dapui").toggle()
+            end,
+            desc = "Debug: Toggle UI",
+        },
+    },
+    config = function()
+        local dap = require("dap")
+        local dapui = require("dapui")
 
-		require("mason-nvim-dap").setup({
-			automatic_installation = true,
-			handlers = {},
-			ensure_installed = {
-				"codelldb",
-				"debugpy",
-				"kotlin-debug-adapter",
-			},
-		})
+        require("mason-nvim-dap").setup({
+            automatic_installation = true,
+            handlers = {},
+            ensure_installed = {
+                "codelldb",
+                "debugpy",
+                "kotlin-debug-adapter",
+            },
+        })
 
-		dapui.setup({
-			icons = { expanded = "▾", collapsed = "▸", current_frame = "*" },
-			controls = {
-				icons = {
-					pause = "⏸",
-					play = "▶",
-					step_into = "⏎",
-					step_over = "⏭",
-					step_out = "⏮",
-					step_back = "b",
-					run_last = "▶▶",
-					terminate = "⏹",
-					disconnect = "⏏",
-				},
-			},
-		})
+        dapui.setup({
+            icons = { expanded = "▾", collapsed = "▸", current_frame = "*" },
+            controls = {
+                icons = {
+                    pause = "⏸",
+                    play = "▶",
+                    step_into = "⏎",
+                    step_over = "⏭",
+                    step_out = "⏮",
+                    step_back = "b",
+                    run_last = "▶▶",
+                    terminate = "⏹",
+                    disconnect = "⏏",
+                },
+            },
+        })
 
-		vim.api.nvim_set_hl(0, "DapBreak", { fg = "#e51400" })
-		vim.api.nvim_set_hl(0, "DapStop", { fg = "#ffcc00" })
-		local breakpoint_icons = vim.g.have_nerd_font
-				and { Breakpoint = "", BreakpointCondition = "", BreakpointRejected = "", LogPoint = "", Stopped = "" }
-			or {
-				Breakpoint = "●",
-				BreakpointCondition = "⊜",
-				BreakpointRejected = "⊘",
-				LogPoint = "◆",
-				Stopped = "⭔",
-			}
-		for type, icon in pairs(breakpoint_icons) do
-			local tp = "Dap" .. type
-			local hl = (type == "Stopped") and "DapStop" or "DapBreak"
-			vim.fn.sign_define(tp, { text = icon, texthl = hl, numhl = hl })
-		end
+        vim.api.nvim_set_hl(0, "DapBreak", { fg = "#e51400" })
+        vim.api.nvim_set_hl(0, "DapStop", { fg = "#ffcc00" })
+        local breakpoint_icons = vim.g.have_nerd_font
+                and { Breakpoint = "", BreakpointCondition = "", BreakpointRejected = "", LogPoint = "", Stopped = "" }
+            or {
+                Breakpoint = "●",
+                BreakpointCondition = "⊜",
+                BreakpointRejected = "⊘",
+                LogPoint = "◆",
+                Stopped = "⭔",
+            }
+        for type, icon in pairs(breakpoint_icons) do
+            local tp = "Dap" .. type
+            local hl = (type == "Stopped") and "DapStop" or "DapBreak"
+            vim.fn.sign_define(tp, { text = icon, texthl = hl, numhl = hl })
+        end
 
-		dap.listeners.after.event_initialized["dapui_config"] = dapui.open
-		dap.listeners.before.event_terminated["dapui_config"] = dapui.close
-		dap.listeners.before.event_exited["dapui_config"] = dapui.close
+        dap.listeners.after.event_initialized["dapui_config"] = dapui.open
+        dap.listeners.before.event_terminated["dapui_config"] = dapui.close
+        dap.listeners.before.event_exited["dapui_config"] = dapui.close
 
-		-- C / C++ / Zig via codelldb
-		dap.adapters.codelldb = {
-			type = "server",
-			port = "${port}",
-			executable = {
-				command = vim.fn.stdpath("data") .. "/mason/bin/codelldb",
-				args = { "--port", "${port}" },
-			},
-		}
-		local codelldb_config = {
-			{
-				name = "Launch",
-				type = "codelldb",
-				request = "launch",
-				program = function()
-					return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
-				end,
-				cwd = "${workspaceFolder}",
-				stopOnEntry = false,
-			},
-		}
-		dap.configurations.c = codelldb_config
-		dap.configurations.cpp = codelldb_config
-		dap.configurations.zig = codelldb_config
+        -- C / C++ / Zig via codelldb
+        dap.adapters.codelldb = {
+            type = "server",
+            port = "${port}",
+            executable = {
+                command = vim.fn.stdpath("data") .. "/mason/bin/codelldb",
+                args = { "--port", "${port}" },
+            },
+        }
+        local codelldb_config = {
+            {
+                name = "Launch",
+                type = "codelldb",
+                request = "launch",
+                program = function()
+                    return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+                end,
+                cwd = "${workspaceFolder}",
+                stopOnEntry = false,
+            },
+        }
+        dap.configurations.c = codelldb_config
+        dap.configurations.cpp = codelldb_config
+        dap.configurations.zig = codelldb_config
 
-		-- Python via debugpy
-		require("dap-python").setup(
-			vim.fn.stdpath("data") .. "/mason/packages/debugpy/venv/bin/python"
-		)
+        -- Python via debugpy
+        require("dap-python").setup(vim.fn.stdpath("data") .. "/mason/packages/debugpy/venv/bin/python")
 
-		-- Kotlin via kotlin-debug-adapter
-		dap.adapters.kotlin = {
-			type = "executable",
-			command = vim.fn.stdpath("data") .. "/mason/bin/kotlin-debug-adapter",
-		}
-		dap.configurations.kotlin = {
-			{
-				type = "kotlin",
-				request = "launch",
-				name = "Launch Kotlin",
-				projectRoot = "${workspaceFolder}",
-				mainClass = function()
-					return vim.fn.input("Main class (e.g. com.example.MainKt): ")
-				end,
-			},
-		}
-	end,
+        -- Kotlin via kotlin-debug-adapter
+        dap.adapters.kotlin = {
+            type = "executable",
+            command = vim.fn.stdpath("data") .. "/mason/bin/kotlin-debug-adapter",
+        }
+        dap.configurations.kotlin = {
+            {
+                type = "kotlin",
+                request = "launch",
+                name = "Launch Kotlin",
+                projectRoot = "${workspaceFolder}",
+                mainClass = function()
+                    return vim.fn.input("Main class (e.g. com.example.MainKt): ")
+                end,
+            },
+        }
+    end,
 }

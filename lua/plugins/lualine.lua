7bb8fe8c49bbe3c2eb13a791180203ccdb4ce3bf
Rares Bogdan <bogdan.andrei.rares@gmail.com>
2026-07-27 20:10:31 +0300

    Update lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,59 +1,235 @@
 return {
     "nvim-lualine/lualine.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
+
+    dependencies = {
+        "nvim-tree/nvim-web-devicons",
+    },
 
     opts = {
         options = {
-            icons_enabled = true,
             theme = "edge",
-            component_separators = { left = "", right = "" },
-            section_separators = { left = "", right = "" },
+            globalstatus = true,
+
+            icons_enabled = true,
+
+            section_separators = {
+                left = "",
+                right = "",
+            },
+
+            component_separators = {
+                left = "│",
+                right = "│",
+            },
+
             disabled_filetypes = {
                 statusline = {},
                 winbar = {},
             },
-            ignore_focus = {},
+
             always_divide_middle = true,
-            always_show_tabline = true,
-            globalstatus = false,
+            always_show_tabline = false,
+
             refresh = {
                 statusline = 100,
                 tabline = 100,
                 winbar = 100,
             },
         },
+
         sections = {
-            lualine_a = { "mode" },
-            lualine_b = { "branch", "diff", "diagnostics" },
-            lualine_c = { "filename" },
-            lualine_x = {
+            ------------------------------------------------------------------
+            -- Mode
+            ------------------------------------------------------------------
+            lualine_a = {
+                {
+                    "mode",
+                    icon = "",
+                },
+            },
+
+            ------------------------------------------------------------------
+            -- Git
+            ------------------------------------------------------------------
+            lualine_b = {
+                {
+                    "branch",
+                    icon = "",
+                },
+                {
+                    "diff",
+                    symbols = {
+                        added = "",
+                        modified = "",
+                        removed = "",
+                    },
+                },
+            },
+
+            ------------------------------------------------------------------
+            -- File
+            ------------------------------------------------------------------
+            lualine_c = {
                 {
                     function()
-                        return require("noice").api.statusline.mode.get()
+                        return vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
                     end,
-                    cond = function()
-                        return package.loaded["noice"] and require("noice").api.statusline.mode.has()
-                    end,
-                    color = { fg = "#ff9e64" },
+                    icon = "",
                 },
-                "encoding",
-                "fileformat",
-                "filetype",
+
+                {
+                    "filename",
+                    path = 1,
+                    symbols = {
+                        modified = " ●",
+                        readonly = " ",
+                        unnamed = "[No Name]",
+                    },
+                },
             },
-            lualine_y = { "progress" },
-            lualine_z = { "location" },
+
+            ------------------------------------------------------------------
+            -- Misc
+            ------------------------------------------------------------------
+            lualine_x = {
+                {
+                    "searchcount",
+                    maxcount = 999,
+                },
+
+                {
+                    "selectioncount",
+                },
+
+                {
+                    function()
+                        local reg = vim.fn.reg_recording()
+
+                        if reg == "" then
+                            return ""
+                        end
+
+                        return "󰑋 @" .. reg
+                    end,
+                },
+
+                {
+                    function()
+                        local ok, dap = pcall(require, "dap")
+
+                        if not ok then
+                            return ""
+                        end
+
+                        local session = dap.session()
+
+                        if not session then
+                            return ""
+                        end
+
+                        if session.stopped_thread_id then
+                            return " Stopped"
+                        end
+
+                        return " Running"
+                    end,
+                },
+
+                {
+                    function()
+                        local clients = vim.lsp.get_clients({
+                            bufnr = 0,
+                        })
+
+                        if #clients == 0 then
+                            return ""
+                        end
+
+                        local names = {}
+
+                        for _, client in ipairs(clients) do
+                            names[#names + 1] = client.name
+                        end
+
+                        return "󰒋 " .. table.concat(names, ", ")
+                    end,
+                },
+
+                {
+                    "filetype",
+                    colored = true,
+                    icon_only = false,
+                },
+            },
+
+            ------------------------------------------------------------------
+            -- Diagnostics
+            ------------------------------------------------------------------
+            lualine_y = {
+                {
+                    "diagnostics",
+                    sources = { "nvim_diagnostic" },
+
+                    symbols = {
+                        error = "",
+                        warn = "",
+                        info = "",
+                        hint = "",
+                    },
+
+                    update_in_insert = false,
+                },
+
+                {
+                    "encoding",
+                },
+
+                {
+                    "fileformat",
+                },
+            },
+
+            ------------------------------------------------------------------
+            -- Cursor
+            ------------------------------------------------------------------
+            lualine_z = {
+                {
+                    "progress",
+                },
+
+                {
+                    "location",
+                },
+            },
         },
+
         inactive_sections = {
             lualine_a = {},
             lualine_b = {},
-            lualine_c = { "filename" },
-            lualine_x = { "location" },
+
+            lualine_c = {
+                {
+                    "filename",
+                    path = 1,
+                },
+            },
+
+            lualine_x = {
+                {
+                    "location",
+                },
+            },
+
             lualine_y = {},
             lualine_z = {},
         },
-        tabline = {},
-        winbar = {},
-        inactive_winbar = {},
-        extensions = {},
+
+        extensions = {
+            "quickfix",
+            "lazy",
+            "mason",
+            "fzf",
+            "nvim-dap-ui",
+        },
     },
 }
